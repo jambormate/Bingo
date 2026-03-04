bfd49b5d6b3e09f8c903d641d7129ba6c6dd0942
Petrik Diak <petrik.diak@petrik.hu>
2026-03-04 08:40:30 +0100

    2-4 feladatrész kész, el vannak tárolva a bingókártyák és ki is írja a feladat, hogy hányan játszanak

---
+++ Bingo/BingoJatekos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+	internal class BingoJatekos
+	{
+		public string Nev { get; set; }
+		public string[,] Kartyanap { get; set; }
+		public BingoJatekos(string nev)
+		{
+			Nev = nev;
+			Kartyanap = new string[5, 5];
+		}
+		public void Kereses(string fajlnev)
+		{
+			string[] sorok = File.ReadAllLines(fajlnev);
+			for (int i = 0; i < 5; i++)
+			{
+				string[] szamok = sorok[i].Split(';');
+				for (int j = 0; j < 5; j++)
+				{
+					Kartyanap[i, j] = szamok[j];
+				}
+			}
+		}
+	}
+}
--- Bingo/Program.cs
+++ Bingo/Program.cs
@@ -2,9 +2,17 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+		static void Main(string[] args)
+		{
+			List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+			string[] jatekosNevek = File.ReadAllLines("nevek.text");
+			foreach (string nev in jatekosNevek)
+			{
+				BingoJatekos jatekos = new BingoJatekos(nev);
+				jatekos.Kereses(nev);
+				jatekosok.Add(jatekos);
+			}
+			Console.WriteLine("4. feladat: A játékosok száma: " + jatekosok.Count);
+		}
+	}
 }
